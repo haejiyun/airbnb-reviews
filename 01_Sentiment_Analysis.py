2c9b83f3fd7268e2b80cafeb9d91ab476d7a9d14
Haeji Yun <88497330+haejiyun@users.noreply.github.com>
2024-08-08 22:32:15 +0200

    Update 01_Sentiment_Analysis.py

--- 01_Sentiment_Analysis.py
+++ 01_Sentiment_Analysis.py
@@ -36,7 +36,7 @@ st.markdown("""
         font-size: 12px;  /* Adjust widget text font size */
     }
     h1 {  /* Title */
-        font-size: 26px;
+        font-size: 30px;
     }
     h2 {  /* Header */
         font-size: 24px;
