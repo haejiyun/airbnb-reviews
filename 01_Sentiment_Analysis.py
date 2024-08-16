7f9f74a0132ebb075cc1a21fc280308aee6edf0f
Haeji Yun <88497330+haejiyun@users.noreply.github.com>
2024-08-16 19:32:28 +0200

    Update 01_Sentiment_Analysis.py

--- 01_Sentiment_Analysis.py
+++ 01_Sentiment_Analysis.py
@@ -42,9 +42,9 @@ st.markdown("""
         font-size: 8.5px;  /* Arrondissement name font size */
     }
     [data-testid="stSidebar"] {
-        width: 300px;  /* Sidebar width */
-        min-width: 300px;  /* Sidebar minimum width */
-        max-width: 300px;  /* Sidebar maximum width */
+        width: 250px;  /* Sidebar width */
+        min-width: 250px;  /* Sidebar minimum width */
+        max-width: 250px;  /* Sidebar maximum width */
     }
     </style>
     """, unsafe_allow_html=True)
