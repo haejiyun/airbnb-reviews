ef1cdc9516fbe00ea5cd66d32b68caca902d275f
Haeji Yun <88497330+haejiyun@users.noreply.github.com>
2024-08-17 16:44:15 +0200

    Update 01_Sentiment_Analysis.py

--- 01_Sentiment_Analysis.py
+++ 01_Sentiment_Analysis.py
@@ -231,7 +231,7 @@ with ClassificationTab: ############################################ Multi-class
                           #title_font_size=17, #Update title configuration
                           #title_xanchor='center',
                           #title_x = 0.5,
-                          #margin=dict(t=0, l=0, r=0, b=20)
+                          margin=dict(t=0, l=0, r=0, b=20)
                           )
         st.markdown("<h5 style='text-align: center; font-weight: normal;'>Multi-Topics of guest reviews</h5>", unsafe_allow_html=True) #Treemap title
         st.plotly_chart(fig, use_container_width=False) #Show the graph
@@ -251,7 +251,7 @@ with ClassificationTab: ############################################ Multi-class
                           xaxis_title=None,
                           yaxis_tickfont=dict(size=15), #Update axis ticks configuration
                           xaxis_tickfont=dict(size=15),
-                          #margin=dict(t=0, l=0, r=0, b=0)
+                          margin=dict(t=0, l=0, r=0, b=0)
                           )
         fig.update_yaxes(tickfont=dict(color='white'),categoryorder='category descending') #Update y-axes configuration
         fig.update_xaxes(tickfont=dict(color='white')) #Update x-axes configuration
