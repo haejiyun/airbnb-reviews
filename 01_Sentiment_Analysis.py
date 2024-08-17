49ae2ad516f5b0e5b43f28fe6bc7c09bf62c736b
Haeji Yun <88497330+haejiyun@users.noreply.github.com>
2024-08-17 03:21:38 +0200

    Update 01_Sentiment_Analysis.py

--- 01_Sentiment_Analysis.py
+++ 01_Sentiment_Analysis.py
@@ -111,7 +111,7 @@ df_filtered = df[mask] #Select filtered data
 
 SentimentTab, ClassificationTab, ReadmeTab = st.tabs(["Sentiment Analysis", "Classification", "Readme"])
 
-#################################################################### Graphs
+#################################################################### Sentiment Analysis
 
 with SentimentTab:
     
@@ -191,3 +191,84 @@ with SentimentTab:
                             xaxis_tickfont=dict(size=13)) #Update x-ticks configuration
     linechart.update_layout(margin=dict(l=60, r=60, t=5, b=115)) #Update margin
     st.plotly_chart(linechart) #Show the chart
+
+
+
+
+
+#################################################################### Classification
+with ClassificationTab:
+
+    #st.write('''
+    #This page presents the results of topic classification for guest reviews across different time periods in Paris. The classification model categorizes comments into five main topics: Apartment, Bed, Communication, Location, Neighborhood. Based on the content of each review, one or multiple relevant topics are assigned. You can customize your view by selecting time period of interest and specific area in Paris.
+    # ''')
+    #st.markdown("***") #Breakline
+    
+    
+    #################################################################### Filters
+    
+    labels_counts = df_filtered['labels_string'].value_counts().reset_index() #Create dataset with count of each combination of topics
+    labels_counts['percent'] = (labels_counts['count'] / labels_counts['count'].sum() * 100).round(2).astype(str) + '%' #Calculate the percentage of each combination
+    labels_counts['label_percent'] = '<b>'+labels_counts['labels_string']+'<br>'+labels_counts['percent'] #Create a column with the name and the percentage of each combination
+    
+    df_exploded = df_filtered.explode('labels') #Create dataset with single topic 
+    df_exploded['labels'] = df_exploded['labels'].str.strip() #Remove space before and after each topic extracted
+    labels_counts_exploded = df_exploded['labels'].value_counts().reset_index() #Count the single topic
+    
+    
+    #################################################################### Graphs
+    
+    col1, col2 = st.columns([3, 2], gap="large") #Create two columns for two graphs
+    
+    with col1: #On the first column
+        fig = px.treemap(labels_counts, #Create a treemap 
+                         path=['label_percent'], 
+                         values='count', 
+                         title='Multi-Topics of guest reviews', 
+                         color='count', 
+                         color_continuous_scale=['#FBD2C5','#00A699'])
+        fig.update_traces(textposition='middle center', #Update text configuration in the treemap
+                          insidetextfont=dict(size=15) 
+                          )
+        fig.update_layout(width=1000, #Update the dimension of the graph
+                          height=550, 
+                          coloraxis_showscale=False, #Hide colorbar
+                          title_font_size=17, #Update title configuration
+                          title_xanchor='center',
+                          title_x = 0.5
+                          )
+        st.plotly_chart(fig, use_container_width=False) #Show the graph
+    
+    with col2: #On the second column
+        fig = px.histogram(labels_counts_exploded, #Create barplot
+                           y='labels', 
+                           x= 'count', 
+                           title='Count of each topics', 
+                           color_discrete_sequence=['#FF5A5F'])
+        fig.update_layout(width = 500, #Update the dimension of the graph
+                          height = 520,
+                          title_font_size=17, #Update the title configuration
+                          title_xanchor='center',
+                          title_x = 0.6,
+                          yaxis_title=None, #Update axis title configuration
+                          xaxis_title=None,
+                          yaxis_tickfont=dict(size=15), #Update axis ticks configuration
+                          xaxis_tickfont=dict(size=15),
+                          )
+        fig.update_yaxes(tickfont=dict(color='white'),categoryorder='category descending') #Update y-axes configuration
+        fig.update_xaxes(tickfont=dict(color='white')) #Update x-axes configuration
+        st.plotly_chart(fig) #Show the plot
+    
+    st.markdown("<h5 style='text-align: center;'>Guest Reviews</h5>", unsafe_allow_html=True) #Title
+    col1, col2, col3 = st.columns([4,2,1]) #Create columns for filters
+    with col1: #On the first column
+        topic = st.multiselect("Select Topics", ['apartment', 'bed', 'communication', 'location', 'neighborhood'], default=['apartment', 'bed', 'communication', 'location', 'neighborhood']) #Create topic selector
+    with col3: #On the last column
+        st.write('') #Blank line
+        if st.button('Refresh comments'): #Create refresh button
+            comments = df_filtered.loc[df_filtered['labels'].apply(lambda x: x == list(sorted(topic))),'comments_en'].sample(5) #If clicked, comments are refreshed
+    
+    comments = df_filtered.loc[df_filtered['labels'].apply(lambda x: x == list(sorted(topic))),'comments_en'].sample(5) #Create 5 random comments which correspond to applied filters
+    with st.container(border=True): #Create a container for the comments
+        for index in range(len(comments)): #Show each comments generated in plain text
+            st.write(index+1, comments.iloc[index])
