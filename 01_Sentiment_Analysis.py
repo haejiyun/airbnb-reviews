01099213fd01635cf59f0744d3e2a78b7bcafb78
Haeji Yun <88497330+haejiyun@users.noreply.github.com>
2024-08-17 11:46:06 +0200

    Update 01_Sentiment_Analysis.py

--- 01_Sentiment_Analysis.py
+++ 01_Sentiment_Analysis.py
@@ -146,7 +146,7 @@ with SentimentTab: ################################################# Sentiment A
                                 color_continuous_scale=['#FF5A5F','#00A699'],
                                 range_color=[0, 5],
                                 mapbox_style="carto-positron",
-                                zoom=11, 
+                                zoom=10, 
                                 center={"lat": 48.86, "lon": 2.345},
                                 #opacity=0.8,
                                 height=350,
