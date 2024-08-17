720fb9aa289ddbb5dddef9c9874388207e422998
Haeji Yun <88497330+haejiyun@users.noreply.github.com>
2024-08-17 18:57:20 +0200

    Update airbnb_streamlit.py

--- airbnb_streamlit.py
+++ airbnb_streamlit.py
@@ -123,9 +123,8 @@ SentimentTab, ClassificationTab, ReadmeTab = st.tabs(["Sentiment Analysis", "Mul
 with SentimentTab: ################################################# Sentiment Analysis
     st.write('''
     This page displays sentiment analysis results for guest reviews of Paris zones across different time periods. Based on the content of each comment, a sentiment score from 1 to 5 is assigned, where 1 represents highly negative sentiment and 5 indicates positive sentiment. You can customize your view by selecting time period of interest and specific area in Paris.  
-    
              ''')
-    
+    st.write("")
     col1, col2 = st.columns([3, 1.5], gap = 'small') #Create two columns for two graphs
     
     with col1: #On the first column
