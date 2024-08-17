84647628b9ffcb98ef59df2bbdb7a4fc8d8a2e1e
Haeji Yun <88497330+haejiyun@users.noreply.github.com>
2024-08-17 19:26:32 +0200

    Update airbnb_streamlit.py

--- airbnb_streamlit.py
+++ airbnb_streamlit.py
@@ -46,7 +46,7 @@ st.markdown("""
         font-size: 15px;  /* Paragraph font size */
     }
     .st-b1 {
-        font-size: 8px !important; /* Arrondissement filter options */
+        font-size: 8.5px !important; /* Arrondissement filter options */
     }
     [data-testid="stSidebar"] {
         width: 250px;  /* Sidebar width */
