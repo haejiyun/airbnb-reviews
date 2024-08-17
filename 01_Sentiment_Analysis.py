308199d8e73d8ec128bc31b5e09e7551145b43af
Haeji Yun <88497330+haejiyun@users.noreply.github.com>
2024-08-17 15:17:43 +0200

    Update 01_Sentiment_Analysis.py

--- 01_Sentiment_Analysis.py
+++ 01_Sentiment_Analysis.py
@@ -223,7 +223,8 @@ with ClassificationTab: ############################################ Multi-class
                          color='count', 
                          color_continuous_scale=['#FBD2C5','#00A699'])
         fig.update_traces(textposition='middle center', #Update text configuration in the treemap
-                          insidetextfont=dict(size=15) 
+                          insidetextfont=dict(size=15),
+                          root_color="rgba(0,0,0,0)"  
                           )
         fig.update_layout(width=1000, #Update the dimension of the graph
                           height=550, 
