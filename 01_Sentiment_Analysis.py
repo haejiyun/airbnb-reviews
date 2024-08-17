2338716b85cb40bf2858fb441a13ca3bcf7bd345
Haeji Yun <88497330+haejiyun@users.noreply.github.com>
2024-08-17 05:58:35 +0200

    Update 01_Sentiment_Analysis.py

--- 01_Sentiment_Analysis.py
+++ 01_Sentiment_Analysis.py
@@ -1,3 +1,4 @@
+#################################################################### LIBRARIES IMPORT
 import pandas as pd
 import numpy as np
 import streamlit as st
@@ -26,8 +27,7 @@ import random
 
 
 
-#################################################################### Streamlit Configuration
-#st.set_option('deprecation.showPyplotGlobalUse', False) #Remove depreciation warning on pyplot
+#################################################################### STREAMLIT CONFIGURATIONS
 st.set_page_config(layout="wide") #Set wide page layout
 
 # Adjust CSS
@@ -56,8 +56,6 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-st.title("Airbnb Guest Reviews in Paris") #Page title
-
 
 
 
@@ -81,34 +79,33 @@ arrondissement_gdf = arrondissement_gdf[['neighbourhood','geometry']] #Select us
 
 
 
-#################################################################### Sidebar Filters
-col1, col2 = st.columns([2, 3], gap = 'medium') #Create two columns for period & arrondissement filters
+#################################################################### PAGE TITLE & SIDEBAR FILTERS
+st.title("Airbnb Guest Reviews in Paris") #Page title
 
-with col1: #On the first column
-    st.sidebar.markdown('## Select Time Period')
-    min_date = df['month'].min().to_pydatetime() #Set minimum date
-    max_date = df['month'].max().to_pydatetime() #Set maximum date
-    selected_min, selected_max = st.sidebar.slider( #Create slider
-        "Period", #Slider name
-        value=(min_date, max_date), #Slider values
-        min_value=min_date, #Minimum value
-        max_value=max_date, #Maximum value
-        format="YYYY-MM" #Format
-    )
+st.sidebar.markdown('## Select Time Period')
+min_date = df['month'].min().to_pydatetime() #Set minimum date
+max_date = df['month'].max().to_pydatetime() #Set maximum date
+selected_min, selected_max = st.sidebar.slider( #Create slider
+    "Period", #Slider name
+    value=(min_date, max_date), #Slider values
+    min_value=min_date, #Minimum value
+    max_value=max_date, #Maximum value
+    format="YYYY-MM" #Format
+)
 
-with col2: #On the second column
-    st.sidebar.markdown('## Select Area')
-    arrondissement_all = ["1 - Louvre","2 - Bourse","3 - Temple","4 - Hôtel-de-Ville","5 - Panthéon",
-                          "6 - Luxembourg","7 - Palais-Bourbon","8 - Élysée","9 - Opéra", "10 - Entrepôt",
-                          "11 - Popincourt","12 - Reuilly","13 - Gobelins","14 - Observatoire","15 - Vaugirard",
-                          "16 - Passy","17 - Batignolles-Monceau","18 - Buttes-Montmartre","19 - Buttes-Chaumont","20 - Ménilmontant"]
-    container = st.sidebar.container() #Create a container for arrondissement filter
-    if 'selected' not in st.session_state: #Create session_state for selected arrondissement
-        st.session_state.selected = arrondissement_all #Set default selection of arrondissement
-    if st.sidebar.button('All arrondissements'): #Create button for all selection
-        st.session_state.selected = arrondissement_all #If the button is clicked, all arrondissement is selected
-    arrondissement = container.multiselect("Arrondissement:", arrondissement_all, default=["1 - Louvre","2 - Bourse","3 - Temple","4 - Hôtel-de-Ville","5 - Panthéon"]) #Create arrondissement mutiselect filter
-    st.session_state.selected = arrondissement #Update selection at each select action is made
+
+st.sidebar.markdown('## Select Area')
+arrondissement_all = ["1 - Louvre","2 - Bourse","3 - Temple","4 - Hôtel-de-Ville","5 - Panthéon",
+                      "6 - Luxembourg","7 - Palais-Bourbon","8 - Élysée","9 - Opéra", "10 - Entrepôt",
+                      "11 - Popincourt","12 - Reuilly","13 - Gobelins","14 - Observatoire","15 - Vaugirard",
+                      "16 - Passy","17 - Batignolles-Monceau","18 - Buttes-Montmartre","19 - Buttes-Chaumont","20 - Ménilmontant"]
+container = st.sidebar.container() #Create a container for arrondissement filter
+if 'selected' not in st.session_state: #Create session_state for selected arrondissement
+    st.session_state.selected = arrondissement_all #Set default selection of arrondissement
+if st.sidebar.button('All arrondissements'): #Create button for all selection
+    st.session_state.selected = arrondissement_all #If the button is clicked, all arrondissement is selected
+arrondissement = container.multiselect("Arrondissement:", arrondissement_all, default=["1 - Louvre","2 - Bourse","3 - Temple","4 - Hôtel-de-Ville","5 - Panthéon"]) #Create arrondissement mutiselect filter
+st.session_state.selected = arrondissement #Update selection at each select action is made
 
 mask = (df['date'] >= selected_min) & (df['date'] <= selected_max) & (df['arrondissement'].isin(arrondissement)) #Create a mask with the filter selection
 df_filtered = df[mask] #Select filtered data
