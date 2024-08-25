6121440d686d56b1bbd61ef91e40fa0a8dcd57c0
Haeji Yun <88497330+haejiyun@users.noreply.github.com>
2024-08-26 00:47:54 +0200

    Update airbnb_streamlit.py

--- airbnb_streamlit.py
+++ airbnb_streamlit.py
@@ -61,9 +61,6 @@ def inject_css():
         svg.st-dv.st-dw.st-eh.st-ei.st-ej.st-ek.st-el {
         display: none !important;
         }
-        .st-bz {
-        margin-top: -10px;  /* Adjust this value as needed */
-        }
         </style>
     """, unsafe_allow_html=True)
 
