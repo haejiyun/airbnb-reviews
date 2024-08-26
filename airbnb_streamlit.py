f9c053a62bb7478971e399c3054fb065846ce0a0
Haeji Yun <88497330+haejiyun@users.noreply.github.com>
2024-08-26 16:07:41 +0200

    Update airbnb_streamlit.py

--- airbnb_streamlit.py
+++ airbnb_streamlit.py
@@ -256,7 +256,7 @@ with ClassificationTab: ############################################ Multi-class
                              color_continuous_scale=['#FBD2C5','#00A699'])
             fig.update_traces(textposition='middle center', #Update text configuration in the treemap
                               insidetextfont=dict(size=15),
-                              marker=dict(cornerradius=5))
+                              marker=dict(cornerradius=2))
             fig.update_layout(width=1000, #Update the dimension of the graph
                               height=400, 
                               coloraxis_showscale=False, #Hide colorbar
