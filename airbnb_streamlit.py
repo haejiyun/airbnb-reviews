dbc3e34e9900527eeed55ee2e5a36bce355fd7d1
Haeji Yun <88497330+haejiyun@users.noreply.github.com>
2024-08-26 15:12:37 +0200

    Update airbnb_streamlit.py

--- airbnb_streamlit.py
+++ airbnb_streamlit.py
@@ -254,10 +254,9 @@ with ClassificationTab: ############################################ Multi-class
                              #title='Multi-Topics of guest reviews', 
                              color='count', 
                              color_continuous_scale=['#FBD2C5','#00A699'])
+            fig.update_traces(root_color="whitesmoke")
             fig.update_traces(textposition='middle center', #Update text configuration in the treemap
-                              insidetextfont=dict(size=15),
-                              root_color="rgba(0,0,0,0)"
-                             )
+                              insidetextfont=dict(size=15))
             fig.update_layout(width=1000, #Update the dimension of the graph
                               height=400, 
                               coloraxis_showscale=False, #Hide colorbar
