711bcbf251cad0b12b1fe196468bee8e1bc01bd0
Haeji Yun <88497330+haejiyun@users.noreply.github.com>
2024-08-26 10:25:01 +0200

    Update airbnb_streamlit.py

--- airbnb_streamlit.py
+++ airbnb_streamlit.py
@@ -118,28 +118,11 @@ with col2:
 container = st.sidebar.container() #Create a container for arrondissement filter
 if 'selected' not in st.session_state: #Create session_state for selected arrondissement
     st.session_state.selected = ["1 - Louvre", "2 - Bourse", "3 - Temple", "4 - Hôtel-de-Ville", "5 - Panthéon", "6 - Luxembourg", "7 - Palais-Bourbon"] #Set default selection of arrondissement
-
-arrondissement = st.session_state.selected
-
-st.session_state.selected = container.multiselect("Select Arrondissement", 
+arrondissement = container.multiselect("Select Arrondissement", 
                                        arrondissement_all, 
                                        default=st.session_state.selected,
                                        label_visibility="collapsed") #Create arrondissement mutiselect filter
-#st.session_state.selected = arrondissement
-
-if arrondissement != st.session_state.selected:
-    raise RerunException(RerunData(widget_state=None))
-
-#arrondissement = container.multiselect("Select Arrondissement", 
-#                                       arrondissement_all, 
-#                                       default=st.session_state.temp_selected,
-#                                       label_visibility="collapsed") #Create arrondissement mutiselect filter
-#st.session_state.temp_selected = arrondissement
-
-# Apply Selection button to finalize changes
-#if st.sidebar.button('Apply Selection'):
-#    st.session_state.selected = st.session_state.temp_selected  # Apply the temporary selection to the final state
-
+st.session_state.selected = arrondissement
 
 mask = (df['date'] >= selected_min) & (df['date'] <= selected_max) & (df['arrondissement'].isin(arrondissement)) #Create a mask with the filter selection
 df_filtered = df[mask] #Select filtered data
