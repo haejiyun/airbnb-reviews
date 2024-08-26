afd04c4671c53249b87e9ee257b4333c3cd76f75
Haeji Yun <88497330+haejiyun@users.noreply.github.com>
2024-08-26 10:10:51 +0200

    Update airbnb_streamlit.py

--- airbnb_streamlit.py
+++ airbnb_streamlit.py
@@ -118,12 +118,17 @@ with col2:
 container = st.sidebar.container() #Create a container for arrondissement filter
 if 'selected' not in st.session_state: #Create session_state for selected arrondissement
     st.session_state.selected = ["1 - Louvre", "2 - Bourse", "3 - Temple", "4 - Hôtel-de-Ville", "5 - Panthéon", "6 - Luxembourg", "7 - Palais-Bourbon"] #Set default selection of arrondissement
-arrondissement = container.multiselect("Select Arrondissement", 
+
+arrondissement = st.session_state.selected
+
+st.session_state.selected = container.multiselect("Select Arrondissement", 
                                        arrondissement_all, 
                                        default=st.session_state.selected,
                                        label_visibility="collapsed") #Create arrondissement mutiselect filter
-st.session_state.selected = arrondissement
+#st.session_state.selected = arrondissement
 
+if arrondissement != session_state.selected:
+    raise RerunException(RerunData(widget_state=None))
 
 #arrondissement = container.multiselect("Select Arrondissement", 
 #                                       arrondissement_all, 
