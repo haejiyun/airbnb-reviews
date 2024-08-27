585d3e7bdc3b3c816d59fe77b979a561e1bd975c
Haeji Yun <88497330+haejiyun@users.noreply.github.com>
2024-08-27 15:36:11 +0200

    Update airbnb_streamlit.py

--- airbnb_streamlit.py
+++ airbnb_streamlit.py
@@ -124,6 +124,11 @@ arrondissement_all = ["1 - Louvre","2 - Bourse","3 - Temple","4 - Hôtel-de-Vill
                       "16 - Passy","17 - Batignolles-Monceau","18 - Buttes-Montmartre","19 - Buttes-Chaumont","20 - Ménilmontant"]
 if 'selected' not in st.session_state: #Create session_state for selected arrondissement
     st.session_state.selected = ["1 - Louvre", "2 - Bourse", "3 - Temple", "4 - Hôtel-de-Ville", "5 - Panthéon", "6 - Luxembourg", "7 - Palais-Bourbon"] #Set default selection of arrondissement
+selection = st.sidebar.radio("Selection",['Select All', 'Deselect All'], label_visability = 'collapsed'])
+if selection = 'Select All':
+    st.session_state.selected = arrondissement_all #If the button is clicked, all arrondissement is selected
+if selection = 'Deselect All':
+        st.session_state.selected = []
 with st.sidebar.form(" "):
     arrondissement = st.multiselect("Select Arrondissement", 
                                    arrondissement_all, 
