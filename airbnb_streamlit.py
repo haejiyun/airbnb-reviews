05fd9fa27abd5505f5bdb27f68ffdf760ecd3607
Haeji Yun <88497330+haejiyun@users.noreply.github.com>
2024-08-27 15:52:12 +0200

    Update airbnb_streamlit.py

--- airbnb_streamlit.py
+++ airbnb_streamlit.py
@@ -116,7 +116,7 @@ selected_min, selected_max = st.sidebar.slider( #Create time filter slider
     format="YYYY-MM-DD", #Format
     label_visibility="collapsed"
 )
-
+st.write("")
 st.sidebar.markdown('#### Select Arrondissement') #Arrondissement Filter title
 arrondissement_all = ["1 - Louvre","2 - Bourse","3 - Temple","4 - Hôtel-de-Ville","5 - Panthéon", 
                       "6 - Luxembourg","7 - Palais-Bourbon","8 - Élysée","9 - Opéra", "10 - Entrepôt",
