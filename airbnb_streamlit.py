99bf1077b63485154ae33bc367895ca7b7bfee07
Haeji Yun <88497330+haejiyun@users.noreply.github.com>
2024-08-28 17:11:14 +0200

    Update airbnb_streamlit.py

--- airbnb_streamlit.py
+++ airbnb_streamlit.py
@@ -148,7 +148,8 @@ with st.sidebar.form(" "):
                                     arrondissement_all, 
                                     default=st.session_state.selected,
                                     label_visibility="collapsed",
-                                    key = "options"
+                                    key = "options",
+                                    on_change = change_option
                                   ) #Create arrondissement mutiselect filter
     if st.form_submit_button("Apply selection"):
         st.session_state.selected = arrondissement
