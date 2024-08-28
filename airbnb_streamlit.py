d8d694d535f9f166734ee61d768299f8b5e1dfa1
Haeji Yun <88497330+haejiyun@users.noreply.github.com>
2024-08-28 16:39:18 +0200

    Update airbnb_streamlit.py

--- airbnb_streamlit.py
+++ airbnb_streamlit.py
@@ -140,12 +140,12 @@ if 'deselect_all_clicked' not in st.session_state:
     st.session_state.deselect_all_clicked = False
 col1, col2 = st.sidebar.columns([0.6, 1], gap = 'small')
 with col1:
-    if st.button('Select All'): #Create button for all selection
+    if button('Select All'): #Create button for all selection
         st.session_state.selected = arrondissement_all #If the button is clicked, all arrondissement is selected
         st.session_state.select_all_clicked = True
         st.session_state.deselect_all_clicked = False
 with col2:
-    if deselect_all = st.button('Deselect All'):
+    if st.button('Deselect All'):
         st.session_state.selected = []
         st.session_state.select_all_clicked = False
         st.session_state.deselect_all_clicked = True
